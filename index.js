f5696a6d4cd884ad14866ecb089a1fc95ef335da
Kushagra Sharma <kushasharma@paypal.com>
2017-01-20 20:28:29 +0530

    finished app

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth', function (err) {
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost:auth/auth', function (err) {
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
